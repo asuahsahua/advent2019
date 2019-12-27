01bd239a3342f4a47b69a9bfb10b7c4031793ad7
asuahsahua <anon@example.com>
2019-12-27 14:47:26 +0000

    Move intcode amplifier code to day 7 - its not necessary to the ongoing operation of the intcode machine

--- cmd/day07/main.go
+++ cmd/day07/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"sync"
 	. "github.com/asuahsahua/advent2019/cmd/common"
-	. "github.com/asuahsahua/advent2019/cmd/intcode"
+	"github.com/asuahsahua/advent2019/cmd/intcode"
 )
 
 func main() {
@@ -42,6 +43,106 @@ func main() {
 
 	bestFeedbackPhase := OptimizeFeedbackProgram(AmplifierControllerSoftware)
 	Part2("%d", FeedbackProgramRun(AmplifierControllerSoftware, bestFeedbackPhase))
+}
+
+type AmpChain struct {
+	Input *chan int64
+	Output *chan int64
+	Chain []*intcode.IntcodeMachine
+}
+
+func AmpProgramRun(prog string, phaseSettings []int64) int64 {
+	amp := NewAmpChain(prog, int64(len(phaseSettings)))
+	amp.PushPhaseSettings(phaseSettings)
+	*amp.Input <- 0
+
+	return amp.Run()
+}
+
+func (amp *AmpChain) PushPhaseSettings(phaseSettings []int64) {
+	// And push the appropriate phase setting to it
+	for i := 0; i < len(phaseSettings); i++ {
+		amp.Chain[i].Input <- phaseSettings[i]
+	}
+}
+
+func (amp *AmpChain) Run() int64 {
+	var wg sync.WaitGroup
+
+	for _, machine := range amp.Chain {
+		wg.Add(1)
+		go func(m *intcode.IntcodeMachine) {
+			m.Run()
+			wg.Done()
+		}(machine)
+	}
+
+	wg.Wait()
+
+	return <- *amp.Output
+}
+
+// Build a chain of amplifiers using the same program
+func NewAmpChain(prog string, size int64) *AmpChain {
+	amps := make([]*intcode.IntcodeMachine, 0)
+	for i := int64(0); i < size; i++ {
+		machine := intcode.NewIntcodeMachineStr(prog)
+		amps = append(amps, machine)
+
+		// Connect the output of the previous with the input of the new
+		if i > 0 {
+			go machine.ReadFrom(amps[i - 1])
+		}
+	}
+
+	return &AmpChain{
+		Input:  &(amps[0].Input),
+		Output: &(amps[size-1].Output),
+		Chain:  amps,
+	}
+}
+
+func FeedbackProgramRun(prog string, phaseSettings []int64) int64 {
+	amp := NewAmpChain(prog, int64(len(phaseSettings)))
+
+	// Need to connect the last output to the input
+	amp.Chain[0].Input = amp.Chain[len(amp.Chain)-1].Output
+	amp.Input = &amp.Chain[0].Input
+
+	amp.PushPhaseSettings(phaseSettings)
+	*amp.Input <- 0
+
+	return amp.Run()
+}
+
+func OptimizeAmpProgram(prog string) []int64 {
+	bestScore := int64(0)
+	bestSlice := make([]int64, 5)
+
+	I64Permutations([]int64{0, 1, 2, 3, 4}, func(perm []int64) {
+		result := AmpProgramRun(prog, perm)
+		if result > bestScore {
+			bestScore = result
+			copy(bestSlice, perm)
+		}
+	})
+
+	return bestSlice
+}
+
+func OptimizeFeedbackProgram(prog string) []int64 {
+	bestScore := int64(0)
+	bestSlice := make([]int64, 5)
+
+	I64Permutations([]int64{5, 6, 7, 8, 9}, func(perm []int64) {
+		result := FeedbackProgramRun(prog, perm)
+		if result > bestScore {
+			bestScore = result
+			copy(bestSlice, perm)
+		}
+	})
+
+	return bestSlice
 }
 
 // The input
